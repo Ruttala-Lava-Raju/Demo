1fd55e12841c9314c426470f14701310d97df9a2
Ruttala-Lava-Raju <lavaraju_ruttala@tecnics.com>
2025-01-18 12:51:39 +0530

    Added sum of number and main function

---
+++ main.py
@@ -0,0 +1,13 @@
+def add_two_numbers(a, b):
+    """Returns the sum of two numbers."""
+    return a + b
+
+def main():
+    """This is the main entry point for the script."""
+    a = 2
+    b = 3
+    c = add_two_numbers(a, b)
+    print(c)
+
+if __name__ == '__main__':
+    main()
